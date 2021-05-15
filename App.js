00115b4bc9f64ad1f606a2d2a092f26c1ea9b92f
whdgkr1127 <79003007+whdgkr1127@users.noreply.github.com>
2021-05-15 23:15:46 +0900

    fokin-weather_Update2

--- App.js
+++ App.js
@@ -1,27 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from "./Loading"
+import * as Location from "expo-location";
 
-export default function App() {
-  return (
-    
-    <View style={styles.container}>
-      <View style={styles.yellowView}></View>
-      <View style={styles.blueView}></View>
-    </View>
-  );
+export default class extends React.Component {
+  render(){
+    return <Loading/>
+  }
+  ;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  yellowView: {
-    flex: 1,
-    backgroundColor:"yellow"
-  },
-  blueView: {
-    flex: 2,
-    backgroundColor:"blue"
-  }
-});
+
