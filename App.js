fc86b0cd7ea6935cdfc139dfc87723ac620ed58b
whdgkr1127 <79003007+whdgkr1127@users.noreply.github.com>
2021-05-15 19:29:04 +0900

    fokin-weather_Update1

--- App.js
+++ App.js
@@ -4,9 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.yellowView}></View>
+      <View style={styles.blueView}></View>
     </View>
   );
 }
@@ -14,8 +15,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  yellowView: {
+    flex: 1,
+    backgroundColor:"yellow"
+  },
+  blueView: {
+    flex: 2,
+    backgroundColor:"blue"
+  }
 });
